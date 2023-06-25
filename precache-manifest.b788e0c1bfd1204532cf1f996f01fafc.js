7dbeae8a461239f62a2aad0548948b9cff55f19d
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-3.local>
2023-06-25 03:45:40 -0700

    Updates

---
+++ precache-manifest.b788e0c1bfd1204532cf1f996f01fafc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ff79a023bbeb76d6cdb",
+    "url": "/static/css/main.fefb4452.chunk.css"
+  },
+  {
+    "revision": "7ff79a023bbeb76d6cdb",
+    "url": "/static/js/main.7ff79a02.chunk.js"
+  },
+  {
+    "revision": "512fd06f35d25e9f6525",
+    "url": "/static/js/1.512fd06f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d5402301e4f60e11e19c9366c561c79",
+    "url": "/static/media/Redaction35-BWXjw.9d540230.otf"
+  },
+  {
+    "revision": "711e41c1602aa46ce91ced970b9608cf",
+    "url": "/static/media/BetonOpen-Regular.711e41c1.otf"
+  },
+  {
+    "revision": "b5f98e34bdaf1e32996a46fccb5884b5",
+    "url": "/static/media/BetonP-ExtBol.b5f98e34.otf"
+  },
+  {
+    "revision": "53f4c3acd65530496009eeec3569b74b",
+    "url": "/static/media/Junicode-Bold.53f4c3ac.ttf"
+  },
+  {
+    "revision": "9ed7601975cb4e738e9f62a74138c91d",
+    "url": "/static/media/Junicode-BoldItalic.9ed76019.ttf"
+  },
+  {
+    "revision": "6ca7e845c5022948ecf891922e4dc5d2",
+    "url": "/static/media/Junicode-Italic.6ca7e845.ttf"
+  },
+  {
+    "revision": "817a5ee346edafd6735edf244de9ed0e",
+    "url": "/static/media/Junicode.817a5ee3.ttf"
+  },
+  {
+    "revision": "d311d48d7387198557998a4dcb4d8ed7",
+    "url": "/index.html"
+  }
+];
